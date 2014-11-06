2680d8b020d7d1626ea4e007fcb3c7b0c645a2e1
Arsalan Ahmed <itsarsalan@yahoo.com>
2014-11-05 22:27:13 -0500

    Moved save and cancel actions on new route from controller

--- app/controllers/picture-albums/new.js
+++ app/controllers/picture-albums/new.js
@@ -1,16 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-	actions: {
-		save: function() {
-			var _this = this;
-			this.get('model').save().then(function(album) {
-				_this.transitionToRoute("picture-albums.show", album);
-			});
-		},
-		cancel: function() {
-			this.transitionToRoute("picture-albums.index");
-			return false;
-		}
-	}
+	
 });
--- app/routes/picture-albums/new.js
+++ app/routes/picture-albums/new.js
@@ -3,5 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function() {
 		return this.store.createRecord('picture-album');
+	},
+	actions: {
+		save: function() {
+			var _this = this;
+			this.modelFor(this.routeName).save().then(function(album) {
+				_this.transitionTo("picture-albums.show", album);
+			});
+		},
+		cancel: function() {
+			this.transitionTo("picture-albums.index");
+			return false;
+		}
 	}
 });
