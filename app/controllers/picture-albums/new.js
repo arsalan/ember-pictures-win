a54f525edb230486e13f6cfe6dceb7bba6ceb747
Arsalan Ahmed <itsarsalan@yahoo.com>
2014-11-09 21:43:21 -0500

    Added ability to add pictures to albums while creating and editing albums

--- app/controllers/picture-albums/new.js
+++ app/controllers/picture-albums/new.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-	
+	actions: {
+		addPicture: function() {
+			this.get('model').set('pictures', this.get('model').pictures || []);
+			this.get('model').get('pictures').pushObject({
+				name: '',
+				description: '',
+				width: 0,
+				height: 0,
+				url :''
+			});
+		}
+	}
 });
